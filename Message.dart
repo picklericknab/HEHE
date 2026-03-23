a368bf42c2cf6163d8293af9c11bb2fb09bcc992
PickleRick! <picklericknab@gmail.com>
2026-03-23 17:11:57 +0800

    Create Message.dart

---
+++ Message.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MessagePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Message Page',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
